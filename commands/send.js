61ac2601aa640296756a44561b45f0c987ed72d0
augustfr <68298953+augustfr@users.noreply.github.com>
2022-11-13 15:29:27 -0800

    change order of inputs

--- commands/send.js
+++ commands/send.js
@@ -3,13 +3,13 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 const sendCommand = new SlashCommandBuilder()
     .setName('send')
     .setDescription('Send currency')
-    .addNumberOption(option => 
-        option.setName('amount')
-            .setDescription('The amount to send')
-            .setRequired(true))
     .addUserOption(option =>
 		option.setName('user')
             .setDescription('User to send to')
+            .setRequired(true))
+    .addNumberOption(option => 
+        option.setName('amount')
+            .setDescription('The amount to send')
             .setRequired(true))
     .addStringOption(option =>
         option.setName('message')
