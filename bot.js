48b70c117a2534975923d413c422ea29e53d1f34
augustfr <68298953+augustfr@users.noreply.github.com>
2022-12-13 13:53:11 -0800

    fix rates formatting

--- bot.js
+++ bot.js
@@ -1263,7 +1263,7 @@ client.on('interactionCreate', async (interaction) => {
               const votes = await tally(serverID)
               acceptVotes(serverID, votes[0].fee, votes[0].income)
               clearVotes(serverID)
-              interaction.editReply({content: votes[0].length + ' votes have been accepted and the new rates are now active.\n\n' + 'New rates:\n' + votes[0].fee + '% transaction fee\n' +  name + votes[0].income + ' daily income', ephemeral: true})
+              interaction.editReply({content: votes[0].length + ' votes have been accepted and the new rates are now active.\n\n' + 'New rates:\n' + votes[0].fee + '% transaction fee\n' + votes[0].income + name + ' shares daily income', ephemeral: true})
             }
           } else {
             interaction.editReply({content: 'Must be server admin', ephemeral: true})
