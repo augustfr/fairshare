14685bb728ef63bb57a71a69f06b704bfc3a1623
augustfr <68298953+augustfr@users.noreply.github.com>
2022-12-23 19:27:51 -0800

    remove candidacy if user leaves server

--- requestChecker.js
+++ requestChecker.js
@@ -1,8 +1,14 @@
 import { config } from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
 import { clearEndorsements, clearRequest } from './bot.js';
+import {
+  Client,
+  GatewayIntentBits,
+} from 'discord.js';
 
 config();
+
+const TOKEN = process.env.TOKEN;
 
 const {
   DATABASE_URL,
@@ -10,6 +16,16 @@ const {
 } = process.env;
 
 const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ],
+});
 
 async function getRequests() {
   const { data, error } = await supabase
@@ -21,15 +37,38 @@ async function getRequests() {
   return {creationTimes, requesterIDs, serverIDs}
 }
 
+async function checkUserID(userID, guildID) {
+  return client.guilds.fetch(guildID)
+    .then(async guild => {
+      return guild.members.fetch(userID)
+        .then(member => {
+          return true;
+        })
+        .catch(error => {
+          return false;
+        })
+    })
+    .catch(error => {
+      return false;
+    })
+}
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export async function checkRequests() {
+
+  client.login(TOKEN);
+
   while (true) {
     const requestList = await getRequests()
     for (let i = 0; i < requestList.requesterIDs.length; i++) {
       if (requestList.creationTimes.length > 0) {
+        if (!(await checkUserID(requestList.requesterIDs[i], requestList.serverIDs[i]))) {
+          clearRequest(requestList.requesterIDs[i], requestList.serverIDs[i])
+          await clearEndorsements(requestList.requesterIDs[i], requestList.serverIDs[i])
+        }
         if ((Date.now() - (new Date(requestList.creationTimes[i]).getTime()) > 604800000)) {
           clearRequest(requestList.requesterIDs[i], requestList.serverIDs[i])
           await clearEndorsements(requestList.requesterIDs[i], requestList.serverIDs[i])
