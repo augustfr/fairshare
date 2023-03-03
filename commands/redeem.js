97eff12d9df076cc75cd4e58e918ce91b331636c
augustfr <68298953+augustfr@users.noreply.github.com>
2023-03-03 12:25:29 -0800

    update desc

--- commands/redeem.js
+++ commands/redeem.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 const redeemCommand = new SlashCommandBuilder()
     .setName('redeem')
-    .setDescription('Vote on transaction fee and daily income')
+    .setDescription('Redeem a payment coupon')
     .addStringOption(option => 
         option.setName('coupon')
             .setDescription('Coupon given from the user')
